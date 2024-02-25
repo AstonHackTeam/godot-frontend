f0f8412821c101a289b3bcb09031418dcd14ab2e
Ryan <121324625+ryanlkl@users.noreply.github.com>
2024-02-25 08:36:11 +0000

    Link to video can now be added

--- Scripts/goose.gd
+++ Scripts/goose.gd
@@ -8,9 +8,14 @@ func _ready():
 	
 func _physics_process(delta):
 	if found:
-		get_tree().quit()
+		open_youtube_link()
 
 func _on_area_2d_body_entered(body):
 	# play video
 	player = body
 	found = true
+
+func open_youtube_link():
+	OS.shell_open("https://www.youtube.com/watch?v=qo8SDCuVCoY")
+	get_tree().quit()
+
